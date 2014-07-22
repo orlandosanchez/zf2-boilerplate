055123a95644f49f236481490590b1be9fccda4b
lando <orlandosanchezjr@gmail.com>
2014-07-21 22:39:51 -0400

    initial commit

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,20 @@
  * file.
  */
 
+
 return array(
-    // ...
+    'db' => array(
+        'driver'         => 'Pdo',
+        'dsn'            => 'mysql:dbname=zf2_boilerplate;host=127.0.0.1',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+                    => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
 );
+
